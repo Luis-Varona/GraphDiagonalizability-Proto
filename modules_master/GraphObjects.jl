546f5779f5baa40c6f647154676cbfe6ad07dde7
Luis Miguel Varona <lbvarona@mta.ca>
2024-09-05 23:39:14 -0300

    Added base of DiagGraph struct (still need methods)

--- modules_master/GraphObjects.jl
+++ modules_master/GraphObjects.jl
@@ -2,6 +2,16 @@ module GraphObjects
     #- EXPORTS AND IMPORTS
     export DiagGraph
     
+    using Intervals: Interval, Closed
+    using LinearAlgebra: Eigen
+    
     
     #- TYPE: `DiagGraph`
+    struct DiagGraph
+        laplacian_matrix::Matrix{Int64}
+        band_zerooneneg::Union{Int64, Interval{Int64, Closed, Closed}}
+        band_oneneg::Union{Int64, Interval{Int64, Closed, Closed}}
+        eigen_zerooneneg::Eigen{T, T, Matrix{T}, Vector{T}} where T<:Union{Missing, Int64}
+        eigen_oneneg::Eigen{τ, τ, Matrix{τ}, Vector{τ}} where τ<:Union{Missing, Int64}
+    end
 end
