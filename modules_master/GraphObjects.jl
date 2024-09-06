7869a30611baadabece5942e3286623fd50a126b
Luis Miguel Varona <lbvarona@mta.ca>
2024-09-06 10:37:55 -0300

    Stable (but unfinished) version of GraphObjects.jl

--- modules_master/GraphObjects.jl
+++ modules_master/GraphObjects.jl
@@ -1,7 +1,13 @@
 module GraphObjects
     #- EXPORTS AND IMPORTS
     export DiagGraph
+    
     using Intervals: Interval, Closed
+    using LinearAlgebra: Diagonal, diag
+    using PythonCall: Py, pyconvert, pyimport, pylist, pytuple
+    
+    np = pyimport("numpy")
+    nx = pyimport("networkx")
     
     #- TYPE: `DiagGraph`
     struct DiagGraph{
@@ -17,5 +23,78 @@ module GraphObjects
         eigvals::Vector{λ}
         eigvecs_zerooneneg::Matrix{τ}
         eigen_oneneg::Matrix{T}
+    end
+    
+    
+    #- String representation of `DiagGraph`
+    function Base.show(io::IO, g::DiagGraph)
+        if g.band_zerooneneg != Inf
+            print(io, "{-1,0,1}-diagonalizable graph on $(g.order) vertices")
+        else
+            print(io, "{-1,0,1}-non-diagonalizable graph on $(g.order) vertices")
+        end
+    end
+    
+    
+    #- Methods on `DiagGraph`
+    function Base.getproperty(g::DiagGraph, prop::Symbol)
+        if prop in (
+            :laplacian_matrix,
+            :band_zerooneneg,
+            :band_oneneg,
+            :eigvals,
+            :eigvecs_zerooneneg,
+            :eigvecs_oneneg
+        )
+            output = getfield(g, prop)
+        
+        elseif prop == :order
+            output = size(g.laplacian_matrix, 1)
+        elseif prop == :size
+            A_unweighted = (g.adjacency_matrix .!= 0)
+            output = Int64(sum(A_unweighted) / 2)
+        elseif prop == :density
+            n = g.order
+            s = g.size
+            output = (s == 1) ? NaN : 2s / (n * (n - 1))
+        elseif prop == :weighted_density
+            n = g.order
+            s = g.size
+            output = (s == 1) ? NaN : sum(g.adjacency_matrix) / (n * (n - 1))
+        elseif prop == :average_degree
+            output = g.size / g.order
+        elseif prop == :average_weighted_degree
+            output = sum(g.adjacency_matrix) / (2g.order)
+        
+        elseif prop == :adjacency_matrix
+            L = g.laplacian_matrix
+            output = Diagonal(L) - L
+        elseif prop == :graph6_string
+            output = networkx_to_graph6(nx.from_numpy_array(np.array(g.adjacency_matrix)))
+        
+        elseif prop == :is_weighted
+            output = any(x -> !(x in [0, 1]), g.adjacency_matrix)
+        elseif prop == :is_negatively_weighted
+            output = any(g.adjacency_matrix .< 0)
+        elseif prop == :is_regular
+            output = allequal(sum(g.adjacency_matrix .!= 0, dims = 2))
+        elseif prop == :is_weighted_regular
+            output = allequal(diag(g.laplacian_matrix))
+        
+        # elseif prop == :is_cograph
+        #     output = _
+        elseif prop == :is_bipartite
+            output = nx.is_bipartite(nx.from_numpy_array(np.array(g.adjacency_matrix)))
+        elseif prop == :is_planar
+            output = nx.is_planar(nx.from_numpy_array(np.array(g.adjacency_matrix)))
+        end
+        
+        return output
+    end
+    
+    
+    #- FUNCTION: `networkx_to_graph6`
+    function networkx_to_graph6(g::Py)
+        return String(pyconvert(Base.CodeUnits, nx.to_graph6_bytes(g)))[11:(end - 1)]
     end
 end
