ed90964ad99d956a3d6a92d8822f827c4207f816
Luis Miguel Varona <lbvarona@mta.ca>
2024-09-06 09:56:13 -0300

    Further specified parametric types for the DiagGraph struct

--- modules_master/GraphObjects.jl
+++ modules_master/GraphObjects.jl
@@ -7,15 +7,15 @@ module GraphObjects
     struct DiagGraph{
         β<:Union{Float64, Int64, Interval{Int64, Closed, Closed}},
         B<:Union{Float64, Int64, Interval{Int64, Closed, Closed}},
-        τ<:Union{Missing, Int64},
+        λ<:Union{Missing, Int64},
+        τ<:Union{Missing, λ},
         T<:Union{Missing, τ},
-        S<:Union{Missing, T},
     }
         laplacian_matrix::Matrix{Int64}
         band_zerooneneg::β
         band_oneneg::B
-        eigvals::Vector{τ}
-        eigvecs_zerooneneg::Matrix{T}
-        eigen_oneneg::Matrix{S}
+        eigvals::Vector{λ}
+        eigvecs_zerooneneg::Matrix{τ}
+        eigen_oneneg::Matrix{T}
     end
 end
